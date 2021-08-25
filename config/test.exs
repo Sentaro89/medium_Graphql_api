f97e6a2487ac7517c7331a83e21d6925f0b45720
Theodorus <theodoredanang@gmail.com>
2021-08-25 13:33:23 +0700

    test Commit

--- config/test.exs
+++ config/test.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # Run `mix help test` for more information.
 config :medium_graphql_api, MediumGraphqlApi.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "rahasia",
   database: "medium_graphql_api_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
